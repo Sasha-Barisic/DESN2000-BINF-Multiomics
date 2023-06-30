8c406a16fdd3b9981acaa8793e69fb817bf18d97
Sasha-Barisic <sashabarisic@gmail.com>
2023-06-30 14:35:23 +1000

    two smpl df generation

--- vccri_dashboard/multiomics_dashboard/clean.py
+++ vccri_dashboard/multiomics_dashboard/clean.py
@@ -144,6 +144,6 @@ def clean_pQ_value(df: pd.DataFrame, cleanQv=False) -> pd.DataFrame:
     new_xdf = new_xdf.reset_index(drop=True)
 
     # # extract from the new_xdf two samples
-    # db1 = new_xdf.filter(regex="CZ|CL1")
-    # db1.to_csv("two_samples.csv", sep=",")
+    # db1 = new_xdf.filter(regex="CZ|CL1|unique_id")
+    # db1.to_csv("two_samples.csv", sep=",", index=False)
     return new_xdf
